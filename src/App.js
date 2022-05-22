757ef25848761187dd6c8c177b97e4fbedaa82a7
RiniChandra7 <rinichandracs0073@gmail.com>
2022-05-23 02:10:05 +0530

    May 22 changes

--- src/App.js
+++ src/App.js
@@ -1,44 +1,43 @@
+import CategoryItem from './components/category-item/category-item.component';
+
+import './categories.styles.scss';
+
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    },
+  ];
+
   return (
-    <div className="categories-container">
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-        <div className="category-container">
-          <div className="category-body-container">
-            <h2>Jackets</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-        <div className="category-container">
-          <div className="category-body-container">
-            <h2>Hats</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-        <div className="category-container">
-          <div className="category-body-container">
-            <h2>Sneakers</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-        <div className="category-container">
-          <div className="category-body-container">
-            <h2>Men</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-        <div className="category-container">
-          <div className="category-body-container">
-            <h2>Women</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      </div>
+    <div className='categories-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
